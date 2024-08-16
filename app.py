df21cfd71d8e6a3198831b2f306f2bd4dbdd80a5
GioEvaristo <giiofonseca08@gmail.com>
2024-08-16 08:37:37 -0300

    16/08

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '9ebe6f92407c97b3f989420e0e6bebcf9d1976b2e230acf9faf4783f5adffe1b'
 
 @app.route('/')
 def index():
@@ -14,6 +14,15 @@ def aula(nome = 'Gabriel', curso = 'Informática', ano = '1'):
     dados = {'nome':nome, 'curso':curso, 'ano':ano}
     return render_template('aula.html', dados_curso=dados)
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/dados', methods=['POST'])
+def dados():
+    flash('Dados Enviados!!!')
+    dados = request.form
+    return render_template('dados.html', dados = dados)
 
 if __name__ == '__main__':
     app.run()
